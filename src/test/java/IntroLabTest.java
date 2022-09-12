32016d771ecf6a5685f4cf350d49fc0f08e6c845
shahi7 <hafsashahid2013@gmail.com>
2022-09-12 19:12:01 -0400

    completed (changed timeout to 100)

--- src/test/java/IntroLabTest.java
+++ src/test/java/IntroLabTest.java
@@ -17,7 +17,7 @@ public class IntroLabTest {
     public void tearDown() {
     }
 
-    @Test(timeout = 10)
+    @Test(timeout = 100)
     public void testExampleStudent() {
         String expected = "Hello! My name is Stu Dent! " +
                 "I like cats more than dogs! Welcome to CSC207!";
@@ -30,4 +30,13 @@ public class IntroLabTest {
      *       the test results.
      * As a reference, we've included testExampleStudent above.
      */
+
+    @Test(timeout = 100)
+    public void testshahi193() {
+        String expected = "Hey! My name is Aziz! " +
+                "I like cats more than dogs! Welcome to CSC207!";
+        String actual = IntroLab.shahi193();
+        assertEquals(expected, actual);
+    }
+
 }
